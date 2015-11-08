247d30bbf54a4b09bee640c15440192de4efac59
Chris Liechti <cliechti@gmx.net>
2015-11-08 23:09:19 +0100

    use upathlib to check for existence of /sd/main.py
    - avoid catching OSErrors that happend when executing the script (and not because of its non-existence)

--- device/flash/main.py
+++ device/flash/main.py
@@ -2,16 +2,18 @@
 
 import expansionboard
 import autoconfig
+import upathlib
 
 autoconfig.wlan()
 
 print('SD: preparing SD card')
 if expansionboard.initialize_sd_card():
+    main = upathlib.Path('/sd/main.py')
     print('SD: mounted to /sd')
-    print('SD: execute /sd/main.py...')
-    try:
-        execfile('/sd/main.py')
-    except OSError: # XXX bad, hides other errors than file not existing
+    print('SD: execute {}...'.format(main))
+    if main.exists():
+        execfile(str(main))
+    else:
         print('SD: no file /sd/main.py found!')
 else:
     print('SD: no card found!')
