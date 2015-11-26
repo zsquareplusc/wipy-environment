d793995ea5e60da687a3c8b697c88f8843ef0d10
Chris Liechti <cliechti@gmx.net>
2015-11-26 23:50:13 +0100

    simplify logging

--- device/flash/lib/autoconfig.py
+++ device/flash/lib/autoconfig.py
@@ -13,29 +13,33 @@ WLAN autoconfiguration
 from network import WLAN
 import time
 
+
+def log(message):
+    print('WLAN:', message)
+
+
 def wlan():
     """Connect in STA mode, fallback to AP"""
     try:
         import wlanconfig
     except ImportError:
-        print('WLAN: no wlanconfig.py')
+        log('no wlanconfig.py')
         wlanconfig = None
         wlan = WLAN(mode=WLAN.AP)
     except Exception as e:
-        print('WLAN: error in wlanconfig.py: {}'.format(e))
+        log('error in wlanconfig.py: {}'.format(e))
         wlanconfig = None
         wlan = WLAN(mode=WLAN.AP)
     else:
         try:
             # configure the WLAN subsystem in station mode (the default is AP)
             wlan = WLAN(mode=WLAN.STA)
-            print('WLAN: connecting to network (AP)...')
+            log('connecting to network (AP)...')
             wlan.connect(wlanconfig.ssid, auth=(WLAN.WPA2, wlanconfig.password), timeout=5000)
-            print('WLAN: waiting for IP...')
+            log('waiting for IP...')
             for tries in range(50):
                 if wlan.isconnected():
-                    print('''\
-WLAN: connected!
+                    log('''connected!
       WiPy IP: {}
       NETMASK: {}
       GATEWAY: {}
@@ -43,10 +47,10 @@ WLAN: connected!
                     break
                 time.sleep_ms(100)
         except OSError:
-            print('WLAN: found no router, going into AP mode instead')
+            log('found no router, going into AP mode instead')
             wlanconfig = None
         except Exception as e:
-            print('WLAN: error: {}'.format(e))
+            log('error: {}'.format(e))
             wlanconfig = None
     if wlanconfig is None:
         wlan.init(mode=WLAN.AP, ssid='wipy-wlan', auth=(WLAN.WPA2,'www.wipy.io'), channel=7, antenna=WLAN.INT_ANT)
