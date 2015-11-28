1a2d272f56a8697098ac2267ba80707a8c3a00df
Chris Liechti <cliechti@gmx.net>
2015-11-28 23:13:50 +0100

    simplify constants, add function to add remote syslog destination

--- device/flash/lib/ulog.py
+++ device/flash/lib/ulog.py
@@ -21,62 +21,63 @@ LOG_INFO      = 6       #  informational
 LOG_DEBUG     = 7       #  debug-level messages
 
 #  facility codes
-LOG_KERN      = 0       #  kernel messages
-LOG_USER      = 1       #  random user-level messages
-LOG_MAIL      = 2       #  mail system
-LOG_DAEMON    = 3       #  system daemons
-LOG_AUTH      = 4       #  security/authorization messages
-LOG_SYSLOG    = 5       #  messages generated internally by syslogd
-LOG_LPR       = 6       #  line printer subsystem
-LOG_NEWS      = 7       #  network news subsystem
-LOG_UUCP      = 8       #  UUCP subsystem
-LOG_CRON      = 9       #  clock daemon
-LOG_AUTHPRIV  = 10      #  security/authorization messages (private)
-LOG_FTP       = 11      #  FTP daemon
-LOG_LOCAL0    = 16      #  reserved for local use
-LOG_LOCAL1    = 17      #  reserved for local use
-LOG_LOCAL2    = 18      #  reserved for local use
-LOG_LOCAL3    = 19      #  reserved for local use
-LOG_LOCAL4    = 20      #  reserved for local use
-LOG_LOCAL5    = 21      #  reserved for local use
-LOG_LOCAL6    = 22      #  reserved for local use
-LOG_LOCAL7    = 23      #  reserved for local use
+LOG_KERN      = 0o000   #  kernel messages
+LOG_USER      = 0o010   #  random user-level messages
+LOG_MAIL      = 0o020   #  mail system
+LOG_DAEMON    = 0o030   #  system daemons
+LOG_AUTH      = 0o040   #  security/authorization messages
+LOG_SYSLOG    = 0o050   #  messages generated internally by syslogd
+LOG_LPR       = 0o060   #  line printer subsystem
+LOG_NEWS      = 0o070   #  network news subsystem
+LOG_UUCP      = 0o100   #  UUCP subsystem
+LOG_CRON      = 0o110   #  clock daemon
+LOG_AUTHPRIV  = 0o120   #  security/authorization messages (private)
+LOG_FTP       = 0o130   #  FTP daemon
 
+prio_text = ('EMERG', 'ALERT', 'CRIT', 'ERR', 'WARN', 'NOTICE', 'INFO', 'DEBUG')
 
 class DefaultHandler(object):
     def __init__(self):
-        self.fmt = '<{p}> {message}\n'
+        self.fmt = '{p} {f} {m}\n'
 
     def write_log(self, facility, priority, message):
-        sys.stdout.write(self.fmt.format(p=(facility << 3) | priority, message=message))
+        sys.stdout.write(self.fmt.format(f=facility, p=prio_text[priority], m=message))
 
 
-class RSyslogHandler(DefaultHandler):
-    #~ """https://tools.ietf.org/html/rfc5424"""
+class RSyslogHandler(object):
+    """https://tools.ietf.org/html/rfc5424"""
     def __init__(self, address):
-        super().__init__()
+        self.fmt = '<{p}> {m}\n'
         self.address = address
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     def write_log(self, facility, priority, message):
-        self.socket.sendto(self.fmt.format(p=(facility << 3) | priority, message=message).encode('utf-8'), self.address)
+        self.socket.sendto(self.fmt.format(p=facility | priority, m=message).encode('utf-8'), self.address)
 
 
-handler = DefaultHandler()
+handlers = [DefaultHandler()]
 
 class Logger(object):
     def __init__(self, facility=LOG_USER):
         self.facility = facility
 
+    def log(self, priority, message):
+        for handler in handlers:
+            handler.write_log(self.facility, priority, message)
+
     def debug(self, message):
-        handler.write_log(self.facility, LOG_DEBUG, message)
+        self.log(LOG_DEBUG, message)
 
     def info(self, message):
-        handler.write_log(self.facility, LOG_INFO, message)
+        self.log(LOG_INFO, message)
 
     def warn(self, message):
-        handler.write_log(self.facility, LOG_WARN, message)
+        self.log(LOG_WARN, message)
 
     def error(self, message):
-        handler.write_log(self.facility, LOG_ERROR, message)
+        self.log(LOG_ERROR, message)
 
+
+def add_remote(self, host, port=514):
+    """Add remote syslog destination"""
+    handlers.append(RSyslogHandler((host, port)))
