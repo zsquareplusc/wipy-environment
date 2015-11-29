9e64bd291eeaea20aaaf2532ea7500afed6c3644
Chris Liechti <cliechti@gmx.net>
2015-11-29 22:30:53 +0100

    ulog: support prefix, root logger

--- device/flash/lib/ulog.py
+++ device/flash/lib/ulog.py
@@ -5,7 +5,7 @@
 #
 # SPDX-License-Identifier:    BSD-3-Clause
 """\
-A small loggin module.
+A small logging module.
 """
 import sys
 import socket
@@ -20,7 +20,7 @@ LOG_NOTICE    = 5       #  normal but significant condition
 LOG_INFO      = 6       #  informational
 LOG_DEBUG     = 7       #  debug-level messages
 
-#  facility codes
+# facility codes
 LOG_KERN      = 0o000   #  kernel messages
 LOG_USER      = 0o010   #  random user-level messages
 LOG_MAIL      = 0o020   #  mail system
@@ -38,32 +38,33 @@ prio_text = ('EMERG', 'ALERT', 'CRIT', 'ERR', 'WARN', 'NOTICE', 'INFO', 'DEBUG')
 
 class DefaultHandler(object):
     def __init__(self):
-        self.fmt = '{p} {f} {m}\n'
+        self.fmt = '{p} {x}{m}\n'
 
-    def write_log(self, facility, priority, message):
-        sys.stdout.write(self.fmt.format(f=facility, p=prio_text[priority], m=message))
+    def write_log(self, facility, priority, prefix, message):
+        sys.stdout.write(self.fmt.format(p=prio_text[priority], x=prefix, m=message))
 
 
 class RSyslogHandler(object):
     """https://tools.ietf.org/html/rfc5424"""
     def __init__(self, address):
-        self.fmt = '<{p}> {m}\n'
+        self.fmt = '<{p}> {x}{m}\n'
         self.address = address
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    def write_log(self, facility, priority, message):
-        self.socket.sendto(self.fmt.format(p=facility | priority, m=message).encode('utf-8'), self.address)
+    def write_log(self, facility, priority, prefix, message):
+        self.socket.sendto(self.fmt.format(p=facility | priority, x=prefix, m=message).encode('utf-8'), self.address)
 
 
 handlers = [DefaultHandler()]
 
 class Logger(object):
-    def __init__(self, facility=LOG_USER):
+    def __init__(self, prefix='', facility=LOG_USER):
+        self.prefix = prefix
         self.facility = facility
 
     def log(self, priority, message):
         for handler in handlers:
-            handler.write_log(self.facility, priority, message)
+            handler.write_log(self.facility, priority, self.prefix, message)
 
     def debug(self, message):
         self.log(LOG_DEBUG, message)
@@ -71,12 +72,17 @@ class Logger(object):
     def info(self, message):
         self.log(LOG_INFO, message)
 
+    def notice(self, message):
+        self.log(LOG_NOTICE, message)
+
     def warn(self, message):
         self.log(LOG_WARN, message)
 
     def error(self, message):
         self.log(LOG_ERROR, message)
 
+
+root = Logger()
 
 def add_remote(self, host, port=514):
     """Add remote syslog destination"""
