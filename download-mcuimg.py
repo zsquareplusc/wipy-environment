b0d6eccfbb84e73884b5c8249c65a4e424e21115
Chris Liechti <cliechti@gmx.net>
2015-11-30 00:56:41 +0100

    do not create empty text file

--- download-mcuimg.py
+++ download-mcuimg.py
@@ -52,10 +52,8 @@ def main():
     else:
         logging.debug('downloading latest official release')
         release_info = json.loads(urllib.request.urlopen('https://api.github.com/repos/wipy/wipy/releases/latest').read().decode('utf-8'))
-        with open('mcuimg.txt', 'w') as f:
-            # pprint(release_info, f)
-            logging.info("TAG: {}, NAME: {}".format(release_info['name'],release_info['tag_name']))
-            zip_url = release_info['assets'][0]['browser_download_url']
+        logging.info("TAG: {}, NAME: {}".format(release_info['name'],release_info['tag_name']))
+        zip_url = release_info['assets'][0]['browser_download_url']
 
     logging.debug('Downloading ZIP from: {}'.format(zip_url))
     with open('Binaries.zip', 'wb') as f:
