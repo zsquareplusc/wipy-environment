50e38171ecdb5fcbfaf526558bd0e251517c2e22
Chris Liechti <cliechti@gmx.net>
2016-10-21 23:21:50 +0200

    feat(scheduler): extend tests

--- test/test_scheduler.py
+++ test/test_scheduler.py
@@ -5,7 +5,9 @@ Tests for task scheduler.
 
 import os
 import sys
+import time
 import traceback
+import threading
 import unittest
 
 if __name__ == '__main__':
@@ -17,8 +19,19 @@ sys.print_exception = lambda e: traceback.print_tb(e.__traceback__) or print(e)
 
 
 class TestScheduler(scheduler.Scheduler):
+    def __init__(self):
+        super().__init__()
+        self.low_power = threading.Event()
+
     def sleep(self):
         print("sleep")
+        self.low_power.clear()
+        self.low_power.wait()
+
+    def wakeup(self):
+        print("wakeup")
+        self.low_power.set()
+
 
 
 class Test_scheduler(unittest.TestCase):
@@ -47,6 +60,53 @@ class Test_scheduler(unittest.TestCase):
         s.run(failure, scheduler.RestartingTask)
         self.assertRaises(scheduler.ExitScheduler, s.loop)
         self.assertEqual(countdown[0], 0)
+
+
+class Test_scheduler_with_interrupts(unittest.TestCase):
+
+    class InterruptSimulator(threading.Thread):
+        """Simulate an interrupt source by using a thread"""
+        def __init__(self, scheduler):
+            super().__init__()
+            self.scheduler = scheduler
+            self.flag = self.scheduler.new_flag()
+            self.alive = True
+
+        def run(self):
+            while self.alive:
+                time.sleep(0.1)
+                print("irq")
+                self.scheduler.set_flag(self.flag)
+
+        def stop(self):
+            self.alive = False
+            self.join(1)
+
+    def setUp(self):
+        """create scheduler and interrupt source"""
+        self.scheduler = TestScheduler()
+        self.irq_thread = self.InterruptSimulator(self.scheduler)
+        self.irq_thread.daemon = True
+        self.irq_thread.start()
+
+    def tearDown(self):
+        self.irq_thread.stop()
+
+    def test_timer_interrupt(self):
+        countdown = [2]  # limit restarts to let test finish...
+        def wait_for_timer():
+            while True:
+                if countdown[0]:
+                    print(countdown[0])
+                    countdown[0] -= 1
+                    yield self.irq_thread.flag
+                else:
+                    raise scheduler.ExitScheduler()
+        self.scheduler.run(wait_for_timer)
+        self.assertRaises(scheduler.ExitScheduler, self.scheduler.loop)
+        self.assertEqual(countdown[0], 0)
+
+
 
 
 if __name__ == '__main__':
