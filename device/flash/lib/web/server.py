42f186e1eb498b87c7c7ec3f0eed44ffbb7ba498
Chris Liechti <cliechti@gmx.net>
2016-11-11 20:46:29 +0100

    feat(web): split Response into more dedicates classes

--- device/flash/lib/web/server.py
+++ device/flash/lib/web/server.py
@@ -13,89 +13,107 @@ from . import umimetypes, url
 gc.collect()
 
 class Response(object):
-    CONTENT_TYPE = b'text/plain'
-    CACHE_CONTROL = {
-        b'Cache-Control': b'no-cache,no-store,must-revalidate',
-    }
 
-    def __init__(self, content=None, stream=None, length=None, status=b'200 OK'):
-        self.content = content
-        self.stream = stream
+    def __init__(self, status):
         self.status = status
         self.headers = {    # mapping of header name -> value
-            b'Content-Type': self.CONTENT_TYPE,
+            b'Cache-Control': b'no-cache,no-store,must-revalidate',
             b'Connection': b'close',
             #~ b'Connection': b'keep-alive',
         }
-        self.headers.update(self.CACHE_CONTROL)
-        if length is None:
-            if self.content is not None:
-                length = len(self.content)
-            elif self.stream is None:
-                length = 0
-        if length is not None:
-            self.headers[b'Content-Length'] = str(length).encode('utf-8')
-
-    def emit_headers(self, server):
-        server.send_response(self.status)
-        for header_name, header_value  in self.headers.items():
-            server.send_header(header_name, header_value)
-        server.end_headers()
 
     def emit_content(self, server):
-        if self.stream is not None:
-            while True:
-                chunk = self.stream.read(256)
-                if not chunk: break
-                server.wfile.write(chunk)
-            try:
-                self.stream.close()
-            except:
-                sys.print_exception()
-            self.stream = None
-        elif self.content is not None:
-            server.wfile.write(self.content.encode('utf-8'))
+        pass
 
     def emit(self, server):
-        self.emit_headers(server)
+        server.send_response(self.status)
+        for header_name, header_value  in self.headers.items():
+            if not isinstance(header_value, (bytes, bytearray, memoryview)):
+                header_value = str(header_value).encode('utf-8')
+            server.send_header(header_name, header_value)
+        server.end_headers()
         self.emit_content(server)
+
+
+class SimpleResponse(Response):
+
+    def __init__(self, content, content_type, status=b'200 OK'):
+        super().__init__(status)
+        if isinstance(content, (bytes, bytearray, memoryview)):
+            self.content = content
+        else:
+            self.content = str(content).encode('utf-8')
+        self.headers[b'Content-Encoding'] = b'utf-8'
+        self.headers[b'Content-Type'] = content_type
+        self.headers[b'Content-Length'] = len(self.content)
+
+    def emit_content(self, server):
+        server.wfile.write(self.content)
+
+
+class StreamResponse(Response):
+
+    def __init__(self, stream, length, content_type, status=b'200 OK'):
+        super().__init__(status)
+        self.stream = stream
+        self.headers[b'Content-Type'] = content_type
+        self.headers[b'Content-Length'] = length
+
+    def emit_content(self, server):
+        while True:
+            chunk = self.stream.read(256)
+            if not chunk: break
+            server.wfile.write(chunk)
+        try:
+            self.stream.close()
+        except:
+            sys.print_exception()
+        self.stream = None
 
 
 class RedirectResponse(Response):
     def __init__(self, url):
-        super().__init__(status=b'302 Moved Temporarily')
+        super().__init__(b'302 Moved Temporarily')
         self.headers[b'Location'] = url.encode('utf-8')  # XXX encoding of url
 
-class HtmlResponse(Response):
-    CONTENT_TYPE = b'text/html'
+
+class HtmlResponse(SimpleResponse):
+    def __init__(self, content, status=b'200 OK'):
+        super().__init__(content, b'text/html', status)
 
 
-class JsonResponse(Response):
-    CONTENT_TYPE = b'application/json'
-
+class JsonResponse(SimpleResponse):
     def __init__(self, obj):
-        Response.__init__(self, json.dumps(obj))
+        super().__init__(json.dumps(obj), b'application/json')
 
 
-class FileResponse(Response):
-    def __init__(self, path):
-        try:
-            s = os.stat(path)
-        except OSError:
-            super().__init__(status=b'404 Not Found')
-        else:
-            fileext = path.split('.')[-1]
-            self.CONTENT_TYPE = umimetypes.type_map.get(fileext, b'application/octet-stream')
-            self.CACHE_CONTROL = {
-                b'Cache-Control': b'max-age: 30',
-            }
-            super().__init__(stream=open(path, 'rb'), length=s[6])
+def FileResponse(path):
+    try:
+        s = os.stat(path)
+    except OSError:
+        return STATUS404
+    else:
+        fileext = path.split('.')[-1]
+        mime_type =  umimetypes.type_map.get(fileext, b'application/octet-stream')
+        response = StreamResponse(open(path, 'rb'), s[6], mime_type)  # stream, len
+        response.headers[b'Cache-Control'] = b'max-age: 30'
+        return response
 
 
-STATUS200 = Response()
-STATUS204 = Response(status=b'204 No Content')
-STATUS500 = Response(status=b'500 Internal Server Error')
-STATUS404 = HtmlResponse(content='<div style="font-size:60">404 Not Found', status=b'404 Not Found')
+class ErrorResponse(HtmlResponse):
+    def __init__(self, status):
+        super().__init__(b'<div style="font-size:60">', status)
+        self.headers[b'Content-Length'] = len(self.content) + len(self.status)
+
+    def emit_content(self, server):
+        server.wfile.write(self.content)
+        server.wfile.write(self.status)
+
+
+STATUS200 = Response(b'200 OK')
+STATUS204 = Response(b'204 No Content')
+STATUS500 = ErrorResponse(b'500 Internal Server Error')
+STATUS404 = ErrorResponse(b'404 Not Found')
 
 
 
